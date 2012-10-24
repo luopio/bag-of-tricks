568405b4a9a3421bf8a3e0d38506b9f1017dc667
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2012-10-24 09:11:11 +0300

    new raf

--- javascript.js
+++ javascript.js
@@ -168,3 +168,29 @@ var q=window.location.search.substring(1);
     window.location="launch/";
   }
 })((navigator.userAgent||navigator.vendor||window.opera));
+
+
+
+
+//=========================================================//
+// requestAnimationFrame polyfill (http://paulirish.com/2011/requestanimationframe-for-smart-animating/)
+//=========================================================//
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+          window.webkitRequestAnimationFrame || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+// usage: 
+// instead of setInterval(render, 16) ....
+(function animloop(){
+  requestAnimFrame(animloop);
+  render();
+})();
+// place the rAF *before* the render() to assure as close to 
+// 60fps with the setTimeout fallback.
