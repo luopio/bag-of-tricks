070a27a80b16d21905fa8bf6bc9d7b567d2b39ae
Lauri Kainulainen <lauri.kainulainen@citat.fi>
2012-11-19 15:35:19 +0200

    new, flexible version of timediff

--- python/timediff.py
+++ python/timediff.py
@@ -3,39 +3,55 @@
 from datetime import datetime
 import re
 
-def calcall(time_str):
-    ''' time_str something like '17:40 - 8:20 - 0:30 - 0:50' '''
-    reg = re.compile('([0-9]{1,2}:[0-9]{2})\s?-?\s?')
-    match = reg.findall(time_str)
-    print match
-    times = []
-    for m in match:
-        t = m.split(':')
-        seconds = int(t[0]) * 60 * 60 + int(t[1]) * 60 
-        times.append(seconds)
-    t = times[0]
-    for tt in times[1:]:
-        t -= tt
-    return t
-
-    # times = time_str.split()
-    # time1 = datetime.strptime(time1, '%H:%M')
+clock_format_reg = re.compile('([0-9]{1,2}):([0-9]{2})')
     
+def calculate(time_str):
+    # test = '17:45 - 30 - 0920 - 45 - 115 + 0:15'
+    buf = ""
+    nums = []
+    last_splitter = time_str[0] if time_str[0] in ('+', '-') else '+'
+    # parse in a stream fashion until the end
+    for i in xrange(len(time_str)):
+        l = time_str[i]
+        if l in ('+', '-'):
+            seconds = extract_seconds(buf)
+            if last_splitter == '-':
+                seconds *= -1
+            nums.append(seconds)
+            buf = ""
+            last_splitter = l
+        else:
+            buf += l
+
+    seconds = extract_seconds(buf)
+    if last_splitter == '-':
+        seconds *= -1       
+    nums.append(seconds)
+    return sum(nums)
 
 
+def extract_seconds(buf):
+    buf = buf.strip()
+    if buf.find(':') != -1:
+        m = clock_format_reg.findall(buf)[0]
+        return int(m[0]) * 60 * 60 + int(m[1]) * 60         
+    elif len(buf) == 2: # e.g. 45 => handle as minutes
+        return int(buf) * 60
+    elif len(buf) == 3:
+        return int(buf[0]) * 60 * 60 + int(buf[1:]) * 60
+    elif len(buf) == 4:
+        return int(buf[0:2]) * 60 * 60 + int(buf[2:]) * 60
+    else:
+        print "(!) dunno how to handle number", buf
+        return 0
+
+
+print 'Time calculation in the form "<TIME> [+|-] <TIME>", where <TIME> can be format HH:MM, HHMM, HMM, MM.'
 while True:
-    t1 = raw_input('Time calculation in the form "HH:MM - HH:MM ... (q to quit)" > ')
+    t1 = raw_input('(q to quit)" > ')
     if t1 == 'q':
         break
-    delta = calcall(t1)
-    print "Difference: %s:%s" % (delta / 60 / 60, delta / 60 % 60)
-
-    # t1 = raw_input('Start time (HH:MM), or q to quit: ')
-    #t2 = raw_input('End time (HH:MM): ')
-    #delta = calcdiff(t1, t2)
-    #if delta:
-    #    print "Difference between: %s:%s" % (delta.seconds / 60 / 60, delta.seconds / 60 % 60)
-    #print
-
+    delta = calculate(t1)
+    print "Result = %s:%s" % (delta / 60 / 60, delta / 60 % 60)
 
 print "Bye. Have a nice day."
